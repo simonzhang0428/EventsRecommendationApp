f3fe7788d21bfdb1e75abbd603a8e48c1073473a
simon <simonzhang0428@gmail.com>
2021-07-29 00:19:51 -0700

    builder pattern

--- src/main/java/entity/Item.java
+++ src/main/java/entity/Item.java
@@ -48,6 +48,21 @@ public class Item {
         return distance;
     }
 
+    /**
+     * This is a builder pattern in Java.
+     */
+    private Item(ItemBuilder builder) {
+        this.itemId = builder.itemId;
+        this.name = builder.name;
+        this.rating = builder.rating;
+        this.address = builder.address;
+        this.categories = builder.categories;
+        this.imageUrl = builder.imageUrl;
+        this.url = builder.url;
+        this.distance = builder.distance;
+    }
+
+
     public JSONObject toJSONObject() {
         JSONObject obj = new JSONObject();
         try {
@@ -63,6 +78,54 @@ public class Item {
             e.printStackTrace();
         }
         return obj;
+    }
+
+    public static class ItemBuilder {
+        private String itemId;
+        private String name;
+        private double rating;
+        private String address;
+        private Set<String> categories;
+        private String imageUrl;
+        private String url;
+        private double distance;
+
+        public void setItemId(String itemId) {
+            this.itemId = itemId;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setRating(double rating) {
+            this.rating = rating;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public void setCategories(Set<String> categories) {
+            this.categories = categories;
+        }
+
+        public void setImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public void setDistance(double distance) {
+            this.distance = distance;
+        }
+
+        public Item build() {
+            return new Item(this);
+        }
+
     }
 
 
